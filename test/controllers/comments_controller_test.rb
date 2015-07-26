f9d503156c2ccf4456648a0883951547ab2e0ec2
benzegreatest <benchinoy@gmail.com>
2015-07-26 20:07:31 +0000

    Latest update

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ test "comment added to database" do
+ 	user = FactoryGirl.create(:user)
+ 	sign_in user
+ 	place = FactoryGirl.create(:place)
+ 	post :create, :place_id => place.id, :comment => {
+ 		:rating => '5_star',
+ 		:message => 'Can we do it sports fans?'
+ 	}
+ 	assert_not_empty(place.comments)
+ 	assert_redirected_to place_path(place)
+end
 end
